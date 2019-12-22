4df7175ec48334c0eaa86ffe6d69339e8914d16a
lisashare <lisa_zhu2012@foxmail.com>
2019-12-22 23:42:08 +0800

    优化页面

---
+++ js/common.js
@@ -0,0 +1,64 @@
+
+let settings = JSON.parse(localStorage.getItem('settings')) || {};
+const Cfg = {
+    designW: settings.designW || 1920, //设计图宽度
+    designH: settings.designH || 1080, //设计图高度
+    zoomMode: settings.zoomMode || (innerWidth < 768 ? 'cover' : 'contain'),
+    notebookOptim: [undefined, true].includes(settings.notebookOptim),
+};
+let hasGetWeather = false;
+let scale = 1;
+let notebookOptim = true;
+let colonShow = true;
+let [pageH, pageW] = [$(window).height(), $(window).width()];
+const Public = {
+    //页面缩放
+    pageResize() {
+        [pageH, pageW] = [$(window).height(), $(window).width()];
+        let isWider = pageW / pageH > Cfg.designW / Cfg.designH;
+        let [scaleW, scaleH] = [pageW / Cfg.designW, pageH / Cfg.designH];
+        let $container = $("#container");
+        switch (Cfg.zoomMode) {
+            case "contain":
+                if (isWider) {
+                    $container.css({width: pageH * Cfg.designW / Cfg.designH, height: '100%'});
+                } else {
+                    $container.css({height: pageW * Cfg.designH / Cfg.designW, width: '100%'});
+                }
+                scale = isWider ? scaleH : scaleW;
+                break;
+            case 'cover':
+                $("html,body").css('overflow', 'initial');
+                if (isWider) {
+                    $container.css({height: pageW * Cfg.designH / Cfg.designW, width: '100%'});
+                } else {
+                    $container.css({width: pageH * Cfg.designW / Cfg.designH, height: '100%'});
+                }
+                scale = isWider ? scaleW : scaleH;
+                break;
+            case 'stretch':
+                scale = isWider ? scaleH : scaleW;
+                $container.css({width: '100%'}, {height: '100%'});
+                break;
+        }
+        $("html").css("font-size", scale * 16 + "px").css("opacity", 1);
+        notebookOptim = !(Cfg.notebookOptim === false || scale > .75);
+        // console.log("~~~~~~~~~窗口高度：" + pageH + ",\n宽度:" + pageW + " \nbody字号：" + scale)
+    },
+    //图表缩放
+    chartsResize(charts, param) {
+        $(window).resize(() => {
+            Object.keys(charts).forEach(id => {
+                if (param && param.notResize.includes(id)) {
+                    return
+                }
+                charts[id].resize();
+            })
+        });
+    },
+};
+
+Public.pageResize();
+$(window).resize(() => {
+    Public.pageResize();
+});
---
+++ mock/data.js
@@ -0,0 +1,54 @@
+
+	var DataList = [
+		{
+		"monthclewconversation": "", // 月线索会话
+		"monthresourceamount": "2", // 月资源总数
+		"monthinvitationamount": "2", // 月邀约总数
+		"monthcomeamount": "2", // 月来访量
+		"monthsignamount": "2", // 月签约量
+		"monthsignsingleamount": "2", // 月签约单量
+		"monthsignprice": "2.00", // 月签约金额
+		"dayresourceamount": "200", // 日资源总量
+		"daycallamount": "2", // 日通话总量
+		"dayinvitationamount": "2", // 日邀约量
+		"daycomeamount": "2", // 日来访量
+		"daysignamount": "2", // 日签约量
+		"daysignsingleamount": "2", // 日签约单量
+		"daysignprice": "10200.00", // 日签约金额
+		"dayserviceonlinenum": "", //  日客服在线量
+		"dayconverastionamount": "", // 日会话总量
+		"dayaccountconsumer": "", // 日账户消费
+		"dayclewconversation": "", // 日线索会话
+		"daytotalsignsingleamount": "", // 日签约单量 (中)
+		"daytotalsignamount": "", // 日签约量(中)
+		"daytotalsignprice": "", // 日签约金额(中)
+		"mapcomevisitamount": [],
+		"mapsignamount": [],
+		"fromchan": "2" // 小物种
+	}, {
+		"monthclewconversation": "",
+		"monthresourceamount": "1000",
+		"monthinvitationamount": "1000",
+		"monthcomeamount": "1000",
+		"monthsignamount": "1000",
+		"monthsignsingleamount": "1000",
+		"monthsignprice": "1000.00",
+		"dayresourceamount": "1000",
+		"daycallamount": "1000",
+		"dayinvitationamount": "1000",
+		"daycomeamount": "1000",
+		"daysignamount": "1000",
+		"daysignsingleamount": "1000",
+		"daysignprice": "1000.00",
+		"dayserviceonlinenum": "",
+		"dayconverastionamount": "",
+		"dayaccountconsumer": "",
+		"dayclewconversation": "",
+		"daytotalsignsingleamount": "",
+		"daytotalsignamount": "",
+		"daytotalsignprice": "",
+		"mapcomevisitamount": [],
+		"mapsignamount": [],
+		"fromchan": "1"
+	}
+];
